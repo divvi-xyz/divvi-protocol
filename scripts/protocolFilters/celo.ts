54ba593b06de162f5de2c8c8e4cfe1bcb78dccf7
Tom McGuire <mcgtom10@gmail.com>
2025-03-17 20:10:37 -0700

    docs: remove invalid todos

--- scripts/protocolFilters/celo.ts
+++ scripts/protocolFilters/celo.ts
@@ -1,6 +1,5 @@
 import { ReferralEvent } from '../types'
 
-// TODO(ENG-200): Implement the filter function
 export async function filter(event: ReferralEvent): Promise<boolean> {
   return !!event
 }
