121702e287f72095363327e38f47f6ce17c0ab4d
Tom McGuire <mcgtom10@gmail.com>
2025-03-19 17:48:25 -0700

    feat: dynamically fetch vaults from somm api

--- scripts/calculateRevenue/protocols/somm/getVaults.ts
+++ scripts/calculateRevenue/protocols/somm/getVaults.ts
@@ -1,8 +1,49 @@
+import { Address, isAddress } from 'viem'
 import { NetworkId } from '../../../types'
+import { fetchWithTimeout } from '../../../utils/fetchWithTimeout'
 import { VaultInfo } from './types'
 
-// TODO(ENG-202): Dynamically fetch vaults from the Somm API
-export function getVaults(): VaultInfo[] {
+export async function getVaults(): Promise<VaultInfo[]> {
+  try {
+    const result = await fetchWithTimeout('https://api.sommelier.finance/tvl')
+    if (!result.ok) {
+      throw new Error(
+        `Failed to fetch vaults from the Somm API: ${result.status} ${result.statusText}`,
+      )
+    }
+
+    const { Response: response } = await result.json()
+    return extractVaultInfo(response)
+  } catch (error) {
+    console.error('Error in getVaults:', error)
+    return getFallbackVaults()
+  }
+}
+
+function extractVaultInfo(response: Record<string, number>): VaultInfo[] {
+  return Object.keys(response)
+    .filter((address) => isValidVaultAddress(address))
+    .map((address) => ({
+      networkId: getNetworkId(address),
+      vaultAddress: address.split('-')[0] as Address,
+    }))
+}
+
+function isValidVaultAddress(address: string): boolean {
+  return (
+    address.endsWith('-arbitrum') ||
+    address.endsWith('-optimism') ||
+    isAddress(address)
+  )
+}
+
+function getNetworkId(address: string): NetworkId {
+  if (address.endsWith('-arbitrum')) return NetworkId['arbitrum-one']
+  if (address.endsWith('-optimism')) return NetworkId['op-mainnet']
+  return NetworkId['ethereum-mainnet']
+}
+
+function getFallbackVaults(): VaultInfo[] {
   return [
     // Real Yield ETH on Arbitrum
     {
