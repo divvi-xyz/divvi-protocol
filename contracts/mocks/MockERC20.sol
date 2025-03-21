05efc2dd98c6cb84c8bfb7c11d7294d07a6f3724
Alex Bakoushin <alex@bakoush.in>
2025-03-21 10:49:23 +0100

    remove burn

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -12,8 +12,4 @@ contract MockERC20 is ERC20 {
   function mint(address account, uint256 amount) external {
     _mint(account, amount);
   }
-
-  function burn(address account, uint256 amount) external {
-    _burn(account, amount);
-  }
 }
