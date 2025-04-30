6fe8205ecf874ca5013bc350c353bc3c3866fcc9
Satish Ravi <satish.ravi@valoraapp.com>
2025-04-30 15:03:33 -0700

    feat: update fetchReferrals to use the new contract

--- scripts/utils/referrals.ts
+++ scripts/utils/referrals.ts
@@ -1,7 +1,20 @@
-import { NETWORK_ID_TO_REGISTRY_ADDRESS } from './networks'
 import { NetworkId, Protocol, ReferralEvent } from '../types'
-import { getRegistryContract } from './index'
-import { Address, hexToString, stringToHex } from 'viem'
+import { getHyperSyncClient } from './index'
+import { Address, decodeEventLog, encodeEventTopics, Hex, pad } from 'viem'
+import { registryContractAbi } from '../../abis/Registry'
+import { BlockField, LogField, Query } from '@envio-dev/hypersync-client'
+import { paginateEventsQuery } from './hypersyncPagination'
+
+const REGISTRY_CONTRACT_ADDRESS = '0xedb51a8c390fc84b1c2a40e0ae9c9882fa7b7277'
+const STAGING_REGISTRY_CONTRACT_ADDRESS =
+  '0x2f5E320698dB89CbefB810Fa19264103d99aAFB1'
+const REWARDS_NETWORK_ID = NetworkId['op-mainnet']
+
+const REGISTRY_START_BLOCK = 134945942 // Block where the registry contract was deployed
+
+const REWARDS_PROVIDERS: Partial<Record<Protocol, Address>> = {
+  'celo-transactions': '0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8', // celo proof of impact
+}
 
 // Remove duplicate events, keeping only the earliest event for each user
 export function removeDuplicates(events: ReferralEvent[]): ReferralEvent[] {
@@ -20,48 +33,88 @@ export function removeDuplicates(events: ReferralEvent[]): ReferralEvent[] {
 
 // Fetch referral events on networks for the given protocol with an optional list of referrer addresses
 export async function fetchReferralEvents(
-  networkIds: NetworkId[],
   protocol: Protocol,
   referrerIds?: Address[],
+  useStaging = false,
 ): Promise<ReferralEvent[]> {
   const referralEvents: ReferralEvent[] = []
   console.log('Fetching referral events for protocol:', protocol)
 
-  await Promise.all(
-    networkIds.map(async (networkId) => {
-      if (!NETWORK_ID_TO_REGISTRY_ADDRESS[networkId]) {
-        return
+  const rewardsProvider = REWARDS_PROVIDERS[protocol]
+
+  if (!rewardsProvider) {
+    console.error('No rewards provider found for protocol:', protocol)
+    return []
+  }
+
+  const registryContractAddress = useStaging
+    ? STAGING_REGISTRY_CONTRACT_ADDRESS
+    : REGISTRY_CONTRACT_ADDRESS
+
+  const topics = encodeEventTopics({
+    abi: registryContractAbi,
+    eventName: 'ReferralRegistered',
+  })
+
+  const hypersyncClient = getHyperSyncClient(REWARDS_NETWORK_ID)
+
+  const hypersyncQuery: Query = {
+    fromBlock: REGISTRY_START_BLOCK,
+    logs: [
+      {
+        address: [registryContractAddress],
+        // topic0 is the event signature for ReferralRegistered
+        // topic2 is the rewards provider
+        topics: [[topics[0]], [], [pad(rewardsProvider)]],
+      },
+    ],
+    fieldSelection: {
+      block: [BlockField.Timestamp],
+      log: [
+        LogField.TransactionHash,
+        LogField.Data,
+        LogField.Topic1,
+        LogField.Topic2,
+        LogField.Topic3,
+      ],
+    },
+  }
+
+  await paginateEventsQuery(
+    hypersyncClient,
+    hypersyncQuery,
+    async (response) => {
+      for (const event of response.data) {
+        if (!event.block) {
+          // should never happen
+          throw new Error(
+            `Block data is missing in the event response: ${JSON.stringify(event)}`,
+          )
+        }
+
+        const decodedEvent = decodeEventLog({
+          abi: registryContractAbi,
+          eventName: 'ReferralRegistered',
+          topics: event.log.topics as [Hex, ...Hex[]],
+          data: event.log.data as Hex,
+        })
+
+        if (
+          referrerIds &&
+          !referrerIds.includes(decodedEvent.args.rewardsConsumer)
+        ) {
+          continue
+        }
+
+        referralEvents.push({
+          protocol,
+          userAddress: decodedEvent.args.user,
+          referrerId: decodedEvent.args.rewardsConsumer,
+          timestamp: Number(event.block.timestamp),
+        })
       }
-      const registryContract = await getRegistryContract(
-        NETWORK_ID_TO_REGISTRY_ADDRESS[networkId],
-        networkId,
-      )
-      const hexProtocol = stringToHex(protocol, { size: 32 })
-
-      const referrers = referrerIds
-        ? referrerIds
-        : ((await registryContract.read.getReferrers([
-            hexProtocol,
-          ])) as Address[])
-
-      await Promise.all(
-        referrers.map(async (referrer) => {
-          const [userAddresses, timestamps] =
-            (await registryContract.read.getUsers([hexProtocol, referrer])) as [
-              string[],
-              number[],
-            ]
-          userAddresses.forEach((userAddress, index) => {
-            referralEvents.push({
-              protocol,
-              userAddress,
-              referrerId: hexToString(referrer, { size: 32 }),
-              timestamp: timestamps[index],
-            })
-          })
-        }),
-      )
-    }),
+    },
   )
+
   return referralEvents
 }
