90cf2e7373cbe3e2e78a10d4b16a18221475e9c7
Satish Ravi <satish.ravi@valoraapp.com>
2025-04-30 23:23:07 -0700

    feat: update fetchReferrals to use the new contract (#114)
    Fixes ENG-322

--- scripts/utils/hypersyncPagination.ts
+++ scripts/utils/hypersyncPagination.ts
@@ -1,4 +1,9 @@
-import { Query, QueryResponse } from '@envio-dev/hypersync-client'
+import {
+  EventResponse,
+  HypersyncClient,
+  Query,
+  QueryResponse,
+} from '@envio-dev/hypersync-client'
 
 export async function paginateQuery(
   client: { get: (query: Query) => Promise<QueryResponse> },
@@ -10,6 +15,34 @@ export async function paginateQuery(
 
   while (hasMoreBlocks) {
     const response = await client.get({ ...query, fromBlock })
+
+    const shouldStop = await onPage(response)
+    if (shouldStop === true) {
+      break
+    }
+
+    if (response.nextBlock === fromBlock) {
+      hasMoreBlocks = false
+    } else {
+      fromBlock = response.nextBlock
+    }
+
+    if (query.toBlock && fromBlock >= query.toBlock) {
+      hasMoreBlocks = false
+    }
+  }
+}
+
+export async function paginateEventsQuery(
+  client: HypersyncClient,
+  query: Query,
+  onPage: (response: EventResponse) => Promise<boolean | void>,
+): Promise<void> {
+  let hasMoreBlocks = true
+  let fromBlock = query.fromBlock
+
+  while (hasMoreBlocks) {
+    const response = await client.getEvents({ ...query, fromBlock })
 
     const shouldStop = await onPage(response)
     if (shouldStop === true) {
