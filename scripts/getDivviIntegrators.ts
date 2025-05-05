c11efddc8a749441b2c87d2867239d08f820296b
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-05 10:31:16 -0600

    implement

--- scripts/getDivviIntegrators.ts
+++ scripts/getDivviIntegrators.ts
@@ -1,20 +1,33 @@
 import { writeFileSync } from 'fs'
 import yargs from 'yargs'
-import { Address } from 'viem'
+import { Address, encodeEventTopics } from 'viem'
 import { LogField } from '@envio-dev/hypersync-client'
 import { paginateQuery } from './utils/hypersyncPagination'
 import { getHyperSyncClient } from './utils'
 import { NetworkId } from './types'
 
 async function getArgs() {
-  const argv = await yargs.env('').option('output-file', {
-    alias: 'o',
-    description: 'output file',
-    type: 'string',
-  }).argv
+  const argv = await yargs
+    .env('')
+    .option('output-file', {
+      alias: 'o',
+      description: 'output file',
+      type: 'string',
+    })
+    .option('reward-pool', {
+      description: 'RewardPool contract address',
+      type: 'string',
+      demandOption: true,
+    })
+    .option('reward-amount', {
+      description: 'reward amount for integration',
+      type: 'string',
+    }).argv
 
   return {
     output: argv['output-file'] ?? `${argv['protocol']}-referrals.csv`,
+    rewardPoolContractAddress: argv['reward-pool'],
+    rewardAmount: argv['reward-amount'],
   }
 }
 
@@ -29,13 +42,19 @@ function removeDuplicates<T>(arr: T[]): T[] {
   })
 }
 
-async function getDivviIntegrators(): Promise<Address[]> {
+async function getDivviIntegrators({
+  rewardPoolContractAddress,
+}: {
+  rewardPoolContractAddress: string
+}): Promise<Address[]> {
   const usersThatHaveIntegrated: Address[] = []
   const usersThatHaveReceivedRewards = new Set<Address>()
+  const usersThatHaveRegisteredAgreements = new Set<Address>()
 
   const queryForIntegrators = {
     logs: [
       {
+        address: ['0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277'], // DivviRegistry production contract
         topics: [
           [
             '0xfddf272d6cdce612f7757626eff4fda5e235d0da62a22cc77ebe3e295b1479d0', // ReferralRegistered topic
@@ -43,18 +62,41 @@ async function getDivviIntegrators(): Promise<Address[]> {
         ],
       },
     ],
-    transactions: [{ from: ['0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277'] }], // DivviRegistry production contract
     fieldSelection: {
       log: [LogField.Topic0, LogField.Topic1, LogField.Topic2, LogField.Topic3],
     },
     fromBlock: 0,
   }
 
+  const ADD_REWARD_TOPIC = encodeEventTopics({
+    abi: rewardPoolAbi,
+    eventName: 'AddReward',
+  })[0]
+
   const queryForRewardsReceivers = {
-    logs: [{ topics: [['0x123']] }], // TODO: Replace with the actual topic for AddReward
-    transactions: [{ from: ['0x123'] }], // TODO: Replace with the actual contract address
+    logs: [
+      { address: [rewardPoolContractAddress], topics: [[ADD_REWARD_TOPIC]] },
+    ],
     fieldSelection: {
       log: [LogField.Topic0, LogField.Topic1],
+    },
+    fromBlock: 0,
+  }
+
+  const queryForRegisteredAgreements = {
+    logs: [
+      {
+        address: ['0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277'], // DivviRegistry production contract
+        topics: [
+          [
+            '0x71ca44fbbd43371f6298c2bef5521b2ade5a42b3239e920cb28a5af430be9bf0', // RewardsAgreementRegistered topic
+          ],
+          ['0x6226ddE08402642964f9A6de844ea3116F0dFc7e'], // Divvi Integration Rewards entity
+        ],
+      },
+    ],
+    fieldSelection: {
+      log: [LogField.Topic0, LogField.Topic1, LogField.Topic2],
     },
     fromBlock: 0,
   }
@@ -72,15 +114,22 @@ async function getDivviIntegrators(): Promise<Address[]> {
         usersThatHaveReceivedRewards.add(transaction.topics[1] as Address)
       }
     }),
+    paginateQuery(client, queryForRegisteredAgreements, async (response) => {
+      for (const transaction of response.data.logs) {
+        usersThatHaveRegisteredAgreements.add(transaction.topics[2] as Address)
+      }
+    }),
   ])
 
   const deduplicatedUsersThatHaveIntegrated = removeDuplicates(
     usersThatHaveIntegrated,
   )
 
-  // TODO: Also filter for if the user is whitelisted
+  // TODO(ENG-345): Also filter for if the user is whitelisted
   const userToReceiveRewards = deduplicatedUsersThatHaveIntegrated.filter(
-    (user: Address) => !usersThatHaveReceivedRewards.has(user),
+    (user: Address) =>
+      !usersThatHaveReceivedRewards.has(user) &&
+      usersThatHaveRegisteredAgreements.has(user),
   )
 
   return userToReceiveRewards
@@ -89,9 +138,16 @@ async function getDivviIntegrators(): Promise<Address[]> {
 async function main() {
   const args = await getArgs()
 
-  const integratorAddresses = await getDivviIntegrators()
+  const integratorAddresses = await getDivviIntegrators({
+    rewardPoolContractAddress: args.rewardPoolContractAddress,
+  })
 
-  writeFileSync(args.output, integratorAddresses.join('\n'))
+  writeFileSync(
+    args.output,
+    integratorAddresses
+      .map((address) => `${address},${args.rewardAmount}`)
+      .join('\n'),
+  )
   console.log(`Wrote results to ${args.output}`)
 }
 
