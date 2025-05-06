c01615709bca345474cf80fe195726ed9f460f25
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-07 05:08:53 +1200

    feat: Add script to get entities that have integrated with Divvi (#117)
    We want to reward entities who have successfully registered a user with
    Divvi. We only want to reward them once.
    
    This script will find the addresses of all entities that have referred a
    user, and have not yet received a reward. It will output these addresses
    into the output csv.
    
    ```
    yarn ts-node ./scripts/getDivviIntegrators.ts --reward-pool=${RewardPool contract address} --reward-amount=${amount in USD}
    ```
    will output a csv of `address, amount` in `divvi-integrator-rewards.csv`

---
+++ scripts/getDivviIntegrators.ts
@@ -0,0 +1,204 @@
+import { writeFileSync } from 'fs'
+import yargs from 'yargs'
+import { Address, encodeEventTopics } from 'viem'
+import { LogField } from '@envio-dev/hypersync-client'
+import { paginateQuery } from './utils/hypersyncPagination'
+import { getHyperSyncClient } from './utils'
+import { NetworkId } from './types'
+import { divviRegistryAbi } from '../abis/DivviRegistry'
+import { rewardPoolAbi } from '../abis/RewardPool'
+import { getNearestBlock } from './calculateRevenue/protocols/utils/events'
+
+const DIVVI_REGISTRY_CONTRACT_ADDRESS =
+  '0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277'
+const DIVVI_INTEGRATION_REWARDS_ENTITY =
+  '0x6226ddE08402642964f9A6de844ea3116F0dFc7e'
+
+async function getArgs() {
+  const argv = await yargs
+    .env('')
+    .option('output-file', {
+      alias: 'o',
+      description: 'output file',
+      type: 'string',
+    })
+    .option('reward-pool', {
+      description: 'RewardPool contract address',
+      type: 'string',
+      demandOption: true,
+    })
+    .option('reward-amount', {
+      description: 'reward amount for integration',
+      type: 'string',
+    })
+    .option('start-timestamp', {
+      alias: 's',
+      description:
+        'inclusive timestamp at which to start checking for integrators (new Date() compatible)',
+      type: 'number',
+      demandOption: true,
+    })
+    .option('end-timestamp', {
+      alias: 'e',
+      description:
+        'exclusive timestamp at which to stop checking for integrators (new Date() compatible)',
+      type: 'number',
+      demandOption: true,
+    }).argv
+
+  return {
+    output: argv['output-file'] ?? 'divvi-integrator-rewards.csv',
+    rewardPoolContractAddress: argv['reward-pool'],
+    rewardAmount: argv['reward-amount'],
+    startTimestamp: new Date(argv['start-timestamp']),
+    endTimestamp: new Date(argv['end-timestamp']),
+  }
+}
+
+function removeDuplicates<T>(arr: T[]): T[] {
+  const seen = new Set<T>()
+  return arr.filter((item) => {
+    if (seen.has(item)) {
+      return false
+    }
+    seen.add(item)
+    return true
+  })
+}
+
+async function getDivviIntegrators({
+  rewardPoolContractAddress,
+  startTimestamp,
+  endTimestamp,
+}: {
+  rewardPoolContractAddress: string
+  startTimestamp: Date
+  endTimestamp: Date
+}): Promise<Address[]> {
+  const consumersThatHaveIntegrated: Address[] = []
+  const consumersThatHaveReceivedRewards = new Set<Address>()
+  const consumersWithDivviIntegrationAgreement = new Set<Address>()
+
+  const REFERRAL_REGISTERED_TOPIC = encodeEventTopics({
+    abi: divviRegistryAbi,
+    eventName: 'ReferralRegistered',
+  })[0]
+
+  const [startBlock, endBlock] = await Promise.all([
+    getNearestBlock(NetworkId['op-mainnet'], startTimestamp),
+    getNearestBlock(NetworkId['op-mainnet'], endTimestamp),
+  ])
+
+  const queryForIntegrators = {
+    fromBlock: startBlock,
+    toBlock: endBlock,
+    logs: [
+      {
+        address: [DIVVI_REGISTRY_CONTRACT_ADDRESS],
+        topics: [[REFERRAL_REGISTERED_TOPIC]],
+      },
+    ],
+    fieldSelection: {
+      log: [LogField.Topic0, LogField.Topic1, LogField.Topic2, LogField.Topic3],
+    },
+  }
+
+  const ADD_REWARD_TOPIC = encodeEventTopics({
+    abi: rewardPoolAbi,
+    eventName: 'AddReward',
+  })[0]
+
+  const queryForRewardsReceivers = {
+    logs: [
+      { address: [rewardPoolContractAddress], topics: [[ADD_REWARD_TOPIC]] },
+    ],
+    fieldSelection: {
+      log: [LogField.Topic0, LogField.Topic1],
+    },
+    fromBlock: 0,
+  }
+
+  const REWARDS_AGREEMENT_REGISTERED_TOPIC = encodeEventTopics({
+    abi: divviRegistryAbi,
+    eventName: 'RewardsAgreementRegistered',
+  })[0]
+
+  const queryForRegisteredDivviIntegrationAgreement = {
+    logs: [
+      {
+        address: [DIVVI_REGISTRY_CONTRACT_ADDRESS],
+        topics: [
+          [REWARDS_AGREEMENT_REGISTERED_TOPIC],
+          [DIVVI_INTEGRATION_REWARDS_ENTITY],
+        ],
+      },
+    ],
+    fieldSelection: {
+      log: [LogField.Topic0, LogField.Topic1, LogField.Topic2],
+    },
+    fromBlock: 0,
+  }
+
+  const client = getHyperSyncClient(NetworkId['op-mainnet'])
+
+  await Promise.all([
+    paginateQuery(client, queryForIntegrators, async (response) => {
+      for (const transaction of response.data.logs) {
+        consumersThatHaveIntegrated.push(transaction.topics[3] as Address)
+      }
+    }),
+    paginateQuery(client, queryForRewardsReceivers, async (response) => {
+      for (const transaction of response.data.logs) {
+        consumersThatHaveReceivedRewards.add(transaction.topics[1] as Address)
+      }
+    }),
+    paginateQuery(
+      client,
+      queryForRegisteredDivviIntegrationAgreement,
+      async (response) => {
+        for (const transaction of response.data.logs) {
+          consumersWithDivviIntegrationAgreement.add(
+            transaction.topics[2] as Address,
+          )
+        }
+      },
+    ),
+  ])
+
+  const deduplicatedConsumersThatHaveIntegrated = removeDuplicates(
+    consumersThatHaveIntegrated,
+  )
+
+  // TODO(ENG-345): Also filter for if the consumer is allowlisted
+  const consumerToReceiveRewards =
+    deduplicatedConsumersThatHaveIntegrated.filter(
+      (consumer: Address) =>
+        !consumersThatHaveReceivedRewards.has(consumer) &&
+        consumersWithDivviIntegrationAgreement.has(consumer),
+    )
+
+  return consumerToReceiveRewards
+}
+
+async function main() {
+  const args = await getArgs()
+
+  const integratorAddresses = await getDivviIntegrators({
+    rewardPoolContractAddress: args.rewardPoolContractAddress,
+    startTimestamp: args.startTimestamp,
+    endTimestamp: args.endTimestamp,
+  })
+
+  writeFileSync(
+    args.output,
+    integratorAddresses
+      .map((address) => `${address},${args.rewardAmount}`)
+      .join('\n'),
+  )
+  console.log(`Wrote results to ${args.output}`)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
