a09eaae54abd5a0434cc118d41b8103a2ab31f4a
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-15 05:45:22 +1200

    feat: Allow referrals to be filtered by allowlisted referrerId (builder) (#138)
    Can now pass a `builder-allowlist-file` csv to the `fetchReferrals`
    script and will filter to only include referrals from allowlisted
    builders.

--- scripts/protocolFilters/celoTransactions.ts
+++ scripts/protocolFilters/celoTransactions.ts
@@ -1,5 +1,14 @@
+import { Address } from 'viem'
 import { ReferralEvent } from '../types'
 
-export async function filter(event: ReferralEvent): Promise<boolean> {
-  return !!event
+const KNOWN_BUILDERS: Address[] = ['0x22886C71a4C1Fa2824BD86210ead1C310B3d7cf5']
+
+export async function filter(
+  event: ReferralEvent,
+  builderAllowList?: Address[],
+): Promise<boolean> {
+  const allAllowlistedBuilders = KNOWN_BUILDERS.concat(builderAllowList ?? [])
+  return allAllowlistedBuilders.some(
+    (address) => address.toLowerCase() === event.referrerId.toLowerCase(),
+  )
 }
