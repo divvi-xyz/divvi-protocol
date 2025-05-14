35dc7265d0bb30a4c86309e7e92945c81d367d00
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-14 16:40:20 -0600

    chore: If no allowlist passed in to celoTransactions, accept all referrals

--- scripts/protocolFilters/celoTransactions.ts
+++ scripts/protocolFilters/celoTransactions.ts
@@ -7,8 +7,9 @@ export async function filter(
   event: ReferralEvent,
   builderAllowList?: Address[],
 ): Promise<boolean> {
-  const allAllowlistedBuilders = KNOWN_BUILDERS.concat(builderAllowList ?? [])
-  return allAllowlistedBuilders.some(
-    (address) => address.toLowerCase() === event.referrerId.toLowerCase(),
-  )
+  return builderAllowList
+    ? KNOWN_BUILDERS.concat(builderAllowList ?? []).some(
+        (address) => address.toLowerCase() === event.referrerId.toLowerCase(),
+      )
+    : true
 }
