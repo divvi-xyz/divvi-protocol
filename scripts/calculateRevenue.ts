fc813a2b82e2ba524bcdaf2245b50ce04817c2f4
Kathy Luo <kathyluo18@gmail.com>
2025-05-14 15:02:05 +0200

    fix: calculateRevenue should look at referral timestamp (#137)
    https://docs.google.com/document/d/1rKIKMtlz3TKFkHXmNqD4B7VH10GB4MUUhl1MKzdnu88/edit?disco=AAABg72mb6E

--- scripts/calculateRevenue.ts
+++ scripts/calculateRevenue.ts
@@ -5,6 +5,9 @@ import { readFileSync, writeFileSync } from 'fs'
 import yargs from 'yargs'
 import { protocols, Protocol } from './types'
 
+// Buffer to account for time it takes for a referral to be registered, since the referral transaction is made first and the referral registration happens on a schedule
+const REFERRAL_TIME_BUFFER_IN_SECONDS = 30 * 60 // 30 minutes
+
 async function main(args: ReturnType<typeof parseArgs>) {
   const inputFile = args['input-file'] ?? `${args['protocol']}-referrals.csv`
   const outputFile = args['output-file'] ?? `${args['protocol']}-revenue.csv`
@@ -23,14 +26,31 @@ async function main(args: ReturnType<typeof parseArgs>) {
   }> = []
 
   for (let i = 0; i < eligibleUsers.length; i++) {
-    const { referrerId, userAddress } = eligibleUsers[i]
+    const { referrerId, userAddress, timestamp } = eligibleUsers[i]
     console.log(
       `Calculating revenue for ${userAddress} (${i + 1}/${eligibleUsers.length})`,
     )
+
+    const referralTimestamp = new Date(
+      timestamp - REFERRAL_TIME_BUFFER_IN_SECONDS,
+    )
+    const endTimestamp = new Date(args['end-timestamp'])
+    if (referralTimestamp.getTime() > endTimestamp.getTime()) {
+      console.log(
+        `Referral date is after end date, skipping ${userAddress} (referral date: ${new Date(timestamp).toISOString()})`,
+      )
+      continue
+    }
+
+    const startTimestamp = new Date(args['start-timestamp'])
     const revenue = await handler({
       address: userAddress,
-      startTimestamp: new Date(args['start-timestamp']),
-      endTimestamp: new Date(args['end-timestamp']),
+      // if the referral happened after the start of the period, only calculate revenue from the referral block onwards so that we exclude user activity before the referral
+      startTimestamp:
+        referralTimestamp.getTime() > startTimestamp.getTime()
+          ? referralTimestamp
+          : startTimestamp,
+      endTimestamp,
     })
     allResults.push({
       referrerId,
