c6a5ae9750fc2a93156d0b5a66ad912a934935fc
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-14 10:05:01 -0600

    add tucop

--- scripts/protocolFilters/celoTransactions.ts
+++ scripts/protocolFilters/celoTransactions.ts
@@ -1,7 +1,7 @@
 import { Address } from 'viem'
 import { ReferralEvent } from '../types'
 
-const KNOWN_BUILDERS: Address[] = []
+const KNOWN_BUILDERS: Address[] = ['0x22886C71a4C1Fa2824BD86210ead1C310B3d7cf5']
 
 export async function filter(
   event: ReferralEvent,
