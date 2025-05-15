e5fb9b70a86e8bfa7f205847f449bbc53613f1e7
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-05-15 15:34:37 -0600

    more implementation

--- scripts/calculateRevenue/protocols/rhino/constants.ts
+++ scripts/calculateRevenue/protocols/rhino/constants.ts
@@ -1,7 +1,18 @@
+import { encodeEventTopics } from "viem"
+import { NetworkId } from "../../../types"
+import { rhinoFiBridgeAbi } from "../../../abis/RhinoFiBridge"
 
+export const TRANSACTION_VOLUME_USD_PRECISION = 8
 
 // Contract addresses on different chains
+export const NETWORK_ID_TO_BRIDGE_CONTRACT_ADDRESS: Partial<Record<NetworkId, string>> = {
+  [NetworkId['base-mainnet']]: '0x2f59E9086ec8130E21BD052065a9E6B2497bb102',
+  [NetworkId['polygon-pos-mainnet']]: '0xBA4EEE20F434bC3908A0B18DA496348657133A7E',
+  [NetworkId['celo-mainnet']]: '0x5e023c31E1d3dCd08a1B3e8c96f6EF8Aa8FcaCd1',
+}
 
 // Deposit event topic
-
-// Withdraw event topic (if looking at both)
+export const BRIDGED_DEPOSIT_WITH_ID_TOPIC = encodeEventTopics({
+  abi: rhinoFiBridgeAbi,
+  eventName: 'BridgedDepositWithId',
+})[0]
--- scripts/calculateRevenue/protocols/rhino/index.ts
+++ scripts/calculateRevenue/protocols/rhino/index.ts
@@ -1,3 +1,117 @@
+import { HypersyncClient, LogField } from '@envio-dev/hypersync-client'
+import { getBlock, getErc20Contract, getHyperSyncClient } from '../../../utils'
+import {
+  BRIDGED_DEPOSIT_WITH_ID_TOPIC,
+  NETWORK_ID_TO_BRIDGE_CONTRACT_ADDRESS,
+  TRANSACTION_VOLUME_USD_PRECISION,
+} from './constants'
+import { NetworkId } from '../../../types'
+import { BridgeTransaction } from './types'
+import { fetchTokenPrices } from '../utils/tokenPrices'
+import { getTokenPrice } from '../beefy'
+import { paginateQuery } from '../../../utils/hypersyncPagination'
+import { Address, fromHex } from 'viem'
+
+async function getUserBridges({
+  address,
+  startTimestamp,
+  endTimestamp,
+  client,
+  networkId,
+}: {
+  address: string
+  contractAddress: string
+  startTimestamp: Date
+  endTimestamp: Date
+  client: HypersyncClient
+  networkId: NetworkId
+}): Promise<BridgeTransaction[]> {
+  const query = {
+    logs: [{ topics: [[BRIDGED_DEPOSIT_WITH_ID_TOPIC]]}],
+    transactions: [{ from: [address] }],
+    fieldSelection: {
+      log: [LogField.BlockNumber, LogField.Data],
+    },
+    fromBlock: 0,
+  }
+
+  const transactions: BridgeTransaction[] = []
+  await paginateQuery(client, query, async (response) => {
+    for (const transaction of response.data.logs) {
+      // Check that the logs contain all necessary fields
+      if (transaction.blockNumber && transaction.data) {
+        const block = await getBlock(networkId, BigInt(transaction.blockNumber))
+        const blockTimestampDate = new Date(Number(block.timestamp) * 1000)
+        // And that the transfer happened within the time window
+        if (
+          blockTimestampDate >= startTimestamp &&
+          blockTimestampDate <= endTimestamp
+        ) {
+          transactions.push({
+            amount: fromHex(transaction.data as Address, 'bigint'), // TODO: Get from data
+            tokenAddress: transaction.address as Address, // TODO: Get from data
+            timestamp: blockTimestampDate,
+          })
+        }
+      } else {
+        console.log('error message')
+      }
+    }
+  })
+  return transactions
+}
+
+export async function getTotalRevenueUsdFromTransactions({
+  userBridges,
+  networkId,
+  startTimestamp,
+  endTimestamp,
+}: {
+  userBridges: BridgeTransaction[]
+  networkId: NetworkId
+  startTimestamp: Date
+  endTimestamp: Date
+}): Promise<number> {
+  if (userBridges.length === 0) {
+    return 0
+  }
+
+  let totalUsdContribution = 0
+
+  // Get the token decimals
+  const tokenId = `${networkId}:${userBridges[0].tokenAddress}`
+  const tokenContract = await getErc20Contract(
+    userBridges[0].tokenAddress,
+    networkId,
+  )
+  const tokenDecimals = BigInt(await tokenContract.read.decimals())
+
+  // Get the historical token prices
+  const tokenPrices = await fetchTokenPrices({
+    tokenId,
+    startTimestamp,
+    endTimestamp,
+  })
+
+  // For each transaction compute the USD contribution and add to the total
+  for (const transaction of userBridges) {
+    const tokenPriceUsd = getTokenPrice(
+      tokenPrices,
+      new Date(transaction.timestamp),
+    )
+    const partialUsdContribution =
+      Number(
+        (transaction.amount *
+          BigInt(tokenPriceUsd * 10 ** TRANSACTION_VOLUME_USD_PRECISION)) /
+          10n ** tokenDecimals,
+      ) /
+      10 ** TRANSACTION_VOLUME_USD_PRECISION
+    totalUsdContribution += partialUsdContribution
+  }
+
+  return totalUsdContribution
+}
+
 export async function calculateRevenue({
   address,
   startTimestamp,
@@ -7,8 +121,27 @@ export async function calculateRevenue({
   startTimestamp: Date
   endTimestamp: Date
 }): Promise<number> {
+  let totalRevenue = 0
   // Use hypersync to get all of the relevant events
-
-  // Use the getTokenPrice logic to get price USD at time of event and convert, aggregate
-  return 0
+  for (const [networkId, contractAddress] of Object.entries(
+    NETWORK_ID_TO_BRIDGE_CONTRACT_ADDRESS,
+  ) as [NetworkId, string][]) {
+    const client = getHyperSyncClient(networkId)
+    const userBridges = await getUserBridges({
+      address,
+      contractAddress,
+      startTimestamp,
+      endTimestamp,
+      client,
+      networkId,
+    })
+    const revenue = await getTotalRevenueUsdFromTransactions({
+      userBridges,
+      networkId,
+      startTimestamp,
+      endTimestamp,
+    })
+    totalRevenue += revenue
+  }
+  return totalRevenue
 }
