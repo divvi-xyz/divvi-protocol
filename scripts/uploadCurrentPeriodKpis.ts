0a9926acc67483292df54d9609d02a0f34a6164c
Kathy Luo <kathyluo18@gmail.com>
2025-05-22 11:42:16 +0200

    fix: throughput

--- scripts/uploadCurrentPeriodKpis.ts
+++ scripts/uploadCurrentPeriodKpis.ts
@@ -132,6 +132,7 @@ async function uploadCurrentPeriodKpis(
       startTimestamp: currentPeriod.startTimestamp,
       endTimestampExclusive,
       outputDir,
+      maxThroughputPerMinute: 250,
     })
     console.log(
       `Calculated kpi's for campaign ${campaign.protocol} in ${Date.now() - calculateKpiStartTime}ms`,
