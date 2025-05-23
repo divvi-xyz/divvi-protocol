b4027afd87f7a00ba7bb888ae2c6b38c1aa2ff43
Kathy Luo <kathyluo18@gmail.com>
2025-05-23 09:24:04 +0200

    chore: remove tests

--- scripts/uploadCurrentPeriodKpis.ts
+++ scripts/uploadCurrentPeriodKpis.ts
@@ -85,10 +85,6 @@ async function uploadCurrentPeriodKpis(
   )
   const endTimestampExclusive = new Date(startOfCalculationHour).toISOString()
 
-  console.log('====endTimestampExclusive', endTimestampExclusive)
-
-  return
-
   const kpiFilePaths: string[] = []
 
   // Due to the DefiLlama API rate limit, there is no point in parallelising the calculations across campaigns
