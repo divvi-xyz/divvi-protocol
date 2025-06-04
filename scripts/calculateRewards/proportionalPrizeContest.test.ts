0232d9cb87a6af4942b9e4da616625bab744ee36
Silas Boyd-Wickizer <silasbw@gmail.com>
2025-06-04 09:44:18 -0700

    chore: return `kpi` from proportional prize contest

--- scripts/calculateRewards/proportionalPrizeContest.test.ts
+++ scripts/calculateRewards/proportionalPrizeContest.test.ts
@@ -26,10 +26,12 @@ describe('calculateProportionalPrizeContest', () => {
       {
         referrerId: 'ref1',
         rewardAmount: '500',
+        kpi: 300n,
       },
       {
         referrerId: 'ref2',
         rewardAmount: '500',
+        kpi: 300n,
       },
     ])
   })
@@ -51,6 +53,7 @@ describe('calculateProportionalPrizeContest', () => {
       {
         referrerId: 'ref2',
         rewardAmount: '1000', // All rewards go to ref2
+        kpi: 100n,
       },
     ])
   })
@@ -92,7 +95,8 @@ describe('calculateSqrtProportionalPrizeContest', () => {
     const rewards = new BigNumber('1000')
     const kpiData = [
       { referrerId: 'ref1', userAddress: 'user1', kpi: '1' },
-      { referrerId: 'ref2', userAddress: 'user2', kpi: '16' },
+      { referrerId: 'ref2', userAddress: 'user2', kpi: '8' },
+      { referrerId: 'ref2', userAddress: 'user3', kpi: '8' },
     ]
 
     const result = calculateSqrtProportionalPrizeContest({
@@ -104,10 +108,12 @@ describe('calculateSqrtProportionalPrizeContest', () => {
       {
         referrerId: 'ref1',
         rewardAmount: '200',
+        kpi: 1n,
       },
       {
         referrerId: 'ref2',
         rewardAmount: '800',
+        kpi: 16n,
       },
     ])
   })
