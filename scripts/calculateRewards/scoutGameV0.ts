578b9aeabde5da24f4742011280b021f4002921a
Joseph Bergeron <jophish126@gmail.com>
2025-06-09 21:27:41 -0400

    Add scout game week 1 reward metadata

--- scripts/calculateRewards/scoutGameV0.ts
+++ scripts/calculateRewards/scoutGameV0.ts
@@ -5,9 +5,9 @@ import { createAddRewardSafeTransactionJSON } from '../utils/createSafeTransacti
 import { calculateSqrtProportionalPrizeContest } from '../../src/proportionalPrizeContest'
 import { KpiRow, ResultDirectory } from '../../src/resultDirectory'
 
-const scoutGameStartTimestamp = new Date('Tue Jun 03 2025 07:00:00 GMT+0000')
+const scoutGameStartTimestamp = new Date('Tue Jun 03 2025 00:00:00 GMT+0000')
 const scoutGameEndTimestampExclusive = new Date(
-  'Fri Jul 02 2025 07:00:00 GMT+0000',
+  'Fri Jul 02 2025 00:00:00 GMT+0000',
 )
 
 const totalRewards = parseEther('180000')
