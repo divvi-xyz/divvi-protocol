38c7e1f4baac97f4aa3da7d802ca1121d1839420
Kathy Luo <kathyluo18@gmail.com>
2025-06-10 16:21:05 +0200

    fix: missing Lisk campaign provider id (#240)
    Without this change the scripts don't pick up the referrals correctly,
    oops 🙈
    
    Related to ENG-415

--- scripts/utils/referrals.ts
+++ scripts/utils/referrals.ts
@@ -18,6 +18,7 @@ const REWARDS_PROVIDERS: Partial<Record<Protocol, Address>> = {
   'celo-transactions': '0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8', // celo proof of impact
   'celo-pg': '0x0423189886D7966f0DD7E7d256898DAeEE625dca',
   'scout-game-v0': '0xC95876688026BE9d6fA7a7c33328bD013efFa2Bb',
+  'lisk-v0': '0x7bEb0e14F8D2e6f6678Cc30d867787B384b19e20',
 }
 
 // Remove duplicate events, keeping only the earliest event for each user
