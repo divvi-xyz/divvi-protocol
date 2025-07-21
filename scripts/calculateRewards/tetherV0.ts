5c5426f669cb1e5b3d45171a5c1cf760680d2f1c
Finnian Jacobson-Schulte <140328381+finnian0826@users.noreply.github.com>
2025-07-21 12:28:17 -0600

    use dot operator

--- scripts/calculateRewards/tetherV0.ts
+++ scripts/calculateRewards/tetherV0.ts
@@ -74,7 +74,7 @@ export async function main(args: ReturnType<typeof parseArgs>) {
 
     totalTransactionsPerReferrer[referrerId] =
       (totalTransactionsPerReferrer[referrerId] ?? 0) +
-      (metadata['totalTransactions'] ?? 0)
+      (metadata.totalTransactions ?? 0)
   }
 
   const rewardsWithMetadata = rewards.map((reward) => ({
