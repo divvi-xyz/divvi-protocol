13019012a435b463a198f66ef9f376fbd8bfd711
Jacob Waterman <jacobrwaterman@gmail.com>
2025-08-05 11:37:53 -0700

    feat(cloud-functions): Add updateDivviEntities endpoint for updating entity metadata (#390)
    https://linear.app/valora/issue/ENG-581/automate-updating-app-names-in-dune
    
    Cloud function that can be called to copy data from the Builder Info
    Google sheet to
    https://storage.googleapis.com/divvi-campaign-data-production/kpi/divvi-entities.json
    which is used in Dune queries

--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -1,8 +1,10 @@
 import { rewardDivviEthCc2025IntegrationV1Endpoint } from './endpoints/rewardDivviEthCc2025IntegrationV1'
 import { redistributeValoraRewards } from './endpoints/redistributeValoraRewards'
+import { updateDivviEntities } from './endpoints/updateDivviEntities'
 
 export = {
   [rewardDivviEthCc2025IntegrationV1Endpoint.name]:
     rewardDivviEthCc2025IntegrationV1Endpoint.handler,
   [redistributeValoraRewards.name]: redistributeValoraRewards.handler,
+  [updateDivviEntities.name]: updateDivviEntities.handler,
 }
